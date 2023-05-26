3d206acf39ad00983868caa94d4818112bf9ea99
Ikenna~ <you@example.com>
2023-05-26 22:14:12 +0100

    final push

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://mongo:27017/mern-goals");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error:${error.message}`);
